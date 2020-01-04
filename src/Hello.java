b940f1cd2986b82e7dc53689b45a6afaaed01a6a
nyarla12 <48872811+nyarla12@users.noreply.github.com>
2020-01-05 00:44:33 +0900

    inital commit

---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+
+	public static void main(String[] args) {
+		boolean power = false;
+		byte b = 127; // byte -128 ~ 127
+		int oct = 010; // 8진수, 10진수 8
+		int hex = 0x10; // 16진수, 10진수로 16
+		long i = 1_000_000_00000L;
+		System.out.println(oct);
+		System.out.println();
+		System.out.println(hex);
+		System.out.println("3 + 4 = " +  7 + 7);
+	}
+}
---
+++ src/Hello2.java
@@ -0,0 +1,7 @@
+public class Hello2 {
+	public static void main(String[] args) {
+		System.out.println("Hello,world");
+	}
+}
+//System.out.println("%d", 0x1A); 10진수로 출력
+//System.out.println("%x", 0x1A); 16진수로 출력
